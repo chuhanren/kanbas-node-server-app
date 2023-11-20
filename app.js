dfa69f3a6fb9c6ff7b8871288b020be7deb1bbb8
Chuhan <chuhan@ChuhandeMacBook-Pro.local>
2023-11-20 18:20:22 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import express from "express";
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./Modules/routes.js";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+Lab5(app);
+CourseRoutes(app);
+ModuleRoutes(app);
+Hello(app);
+app.listen(process.env.PORT || 4000);
