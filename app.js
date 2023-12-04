9d0c135aa0c367277222ec14792127f3b898635f
Chuhan <chuhan@ChuhandeMacBook-Pro.local>
2023-12-04 17:51:54 -0500

    a6 mongo 23fall

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const app = express();
 const CONNECTION_STRING =
   process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
 mongoose.connect(CONNECTION_STRING);
+
 app.use(
   cors({
     credentials: true,
